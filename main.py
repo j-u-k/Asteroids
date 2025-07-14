a818247e031c37289c1f55b3589387cb2f1c6492
j-u-k <jukhan@outlook.com>
2025-07-14 03:22:30 +0100

    The Player character has now been setup

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill
-        pygame.display.flip
-        
+        screen.fill("black")
+
+        for drawables in drawable:
+            drawables.draw(screen)
+
+        updatable.update(dt)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
     print (f"""
            Starting Asteroids!
